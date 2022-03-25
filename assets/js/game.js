2bcfd27743d8d8cacf5890aa14be1d4fca3ab6bb
Blake Smethers <smethersblake@gmail.com>
2022-03-25 10:23:54 -0500

    added highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -105,13 +105,25 @@ var startGame = function (){
     endGame();
 }
 var endGame = function(){
-    // if player is still alive, player wins!
     if (playerInfo.health > 0) {
         window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
     }
     else {
         window.alert("You've lost your robot in battle.")
     }
+    window.alert("The game has now ended. Let's see how you did!");
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    
     // ask player if they'd like to play again?
     var playAgainConfirm = window.confirm("Would you like to play again?")
 
